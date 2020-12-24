c36491aeb5bfefcff0efabccf6d5753117690c86
Shaun Jorstad <jorstadsd17@gcc.edu>
2020-12-24 16:19:14 -0500

    initial structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// import Header from './components/Header.js';
+// import About from './components/About.js';
+// import Projects from './components/Projects.js';
+// import Jobs from './components/Jobs.js';
+// import Skills from './components/Skills.js';
+// import Footer from './components/Footer.js';
+
+function App() {
+  return (
+    <div>
+      Testing
+    </div>
+  );
+}
+
+export default App;
