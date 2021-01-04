b26d3a3273a83518d83e24490922392a451e213d
Shaun Jorstad <jorstadsd17@gcc.edu>
2021-01-04 17:22:36 -0500

    updates content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ class Footer extends React.Component {
         content={
           <div className="mb-20">
             <p className="opacity-7 lato-light text-lg">I'm always looking to meet new people, share new experiences and code new things!</p>
-            <p className="opacity-7 lato-light text-lg">I am currently seeking a remote front end software engineering position following my graduation in May. Please connect if i would be a suitable candidate to join your team!</p>
+            <p className="opacity-7 lato-light text-lg">I am currently seeking a remote front end software engineering position following my graduation in May. Please connect if I would be a suitable candidate to join your team!</p>
 
             <div className="mt-5 grid grid-cols-7">
               <a className="text-primary" href="https://www.github.com/shaunjorstad">Github</a>
